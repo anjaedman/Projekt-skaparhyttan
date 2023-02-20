1fa1f244e0e8624ade4f23557af302ccf33e34b2
anjaedman <113342171+anjaedman@users.noreply.github.com>
2023-02-20 14:33:24 +0100

    updatering 20/2

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 
 // kontaktformulär
-
 const inputs = document.querySelectorAll(".input");
 
 function focusFunc() {
@@ -18,4 +17,20 @@ function blurFunc() {
 inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
+});
+
+// mail ai 
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const phone = form.elements.phone.value;
+  const message = form.elements.message.value;
+
+  const mailto = `mailto:skaparhyttan@outlook.com?subject=Message from ${name} (${email}) ${phone} &body=${message}`;
+  window.location.href = mailto;
 });
--- js/kontakt.js
+++ js/kontakt.js
@@ -16,3 +16,19 @@ inputs.forEach((input) => {
   input.addEventListener("focus", focusFunc);
   input.addEventListener("blur", blurFunc);
 });
+
+// mail ai 
+
+const form = document.querySelector('#contact-form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = form.elements.name.value;
+  const email = form.elements.email.value;
+  const phone = form.elements.phone.value;
+  const message = form.elements.message.value;
+
+  const mailto = `mailto:skaparhyttan@outlook.com?subject=Message from ${name} (${email}) ${phone} &body=${message}`;
+  window.location.href = mailto;
+});
